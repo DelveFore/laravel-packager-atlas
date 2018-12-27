23df074a364092d943e9feea8fb94122c2a107e1
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2018-12-27 16:24:24 -0600

    For starter purposes I'm adding Hermes and instructions

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        DelveFore\PackagerHermes\PackagerHermesServiceProvider::class,
 
         /*
          * Application Service Providers...
